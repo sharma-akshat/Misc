d310df93fa773dce6a96963cdc94e7f6dbfd73a5
sharma-akshat <akshatsharma0905@gmail.com>
2021-11-12 11:37:49 +0530

    commit

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class MatrixMultiplication
 {
     static void printMatrix(int M[][],int rowSize,int colSize)
@@ -51,55 +51,6 @@ class MatrixMultiplication
         };
         multiplyMatrix(row1,col1,row2,col2,A,B);
     }
-=======
-class MatrixMultiplication
-{
-    static void printMatrix(int M[][],int rowSize,int colSize)
-    {
-        for(int i=0;i<rowSize;i++)
-        {
-            for(int j=0;j<colSize;j++)
-            {
-                System.out.print(M[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-    static void multiplyMatrix(int row1,int col1,int row2,int col2,int A[][],int B[][])
-    {
-        System.out.println("Matrix A is: ");
-        printMatrix(A,row1,col1);
-        System.out.println("Matrix B is: ");
-        printMatrix(B,row2,col2);
-        int C[][]=new int[row1][col2];
-        for(int i=0;i<row1;i++)
-        {
-            for(int j=0;j<col2;j++)
-            {
-                for(int k=0;k<row2;k++)
-                {
-                    C[i][j]+=A[i][k]*B[k][j];
-                }
-            }
-        }
-        System.out.println("Resultant Matrix is: ");
-        printMatrix(C,row1,col2);
-    }
-    public static void main(String gg[])
-    {
-        int row1=4, col1=4, row2=4, col2=4;
-        int A[][]={
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,10,11,12},
-            {13,14,15,16}};
-        int B[][]={
-            {10,20,30,40},
-            {50,60,70,80},
-            {90,100,110,120},
-            {130,140,150,160}};
-        multiplyMatrix(row1,col1,row2,col2,A,B);
-    }
->>>>>>> 7407958d0e3f377871b22f3f63523e128d4f2f9e
+
+
 }
--- MinimumAndMaximum.java
+++ MinimumAndMaximum.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.*;
 class MinimumAndMaximum
 {
@@ -38,45 +38,6 @@ class MinimumAndMaximum
         System.out.println("Minimum number is : "+min);
         sc.close();
     }
-=======
-import java.util.*;
-class MinimumAndMaximum
-{
-    public int findMaxNumber(int[] x)
-    {
-        int maximum=x[0];
-        for(int i=1;i<x.length;i++)
-        {
-            if(x[i]>maximum) maximum=x[i];
-        }
-        return maximum;
-    }
-    public int findMinNumber(int[] y)
-    {
-        int minimum=y[0];
-        for(int i=1;i<y.length;i++)
-        {
-            if(y[i]<minimum) minimum=y[i];
-        }
-        return minimum;
-    }
-    public static void main(String gg[])
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the size of array : ");
-        int size=sc.nextInt();
-        int array[]=new int[size];
-        for(int i=0;i<size;i++)
-        {
-            System.out.print("Enter the element : ");
-            array[i]=sc.nextInt();
-        }
-        MinimumAndMaximum mm=new MinimumAndMaximum();
-        int max=mm.findMaxNumber(array);
-        int min=mm.findMinNumber(array);
-        System.out.println("Maximum number is : "+max);
-        System.out.println("Minimum number is : "+min);
-        sc.close();
-    }
->>>>>>> 7407958d0e3f377871b22f3f63523e128d4f2f9e
+
+
 }
